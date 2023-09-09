4b720ea7b6f0b33007686aaa8941c34574748bcb
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-09-09 14:58:29 +0700

    feat: finish basic structure + styling

--- 01-Dasar-Web/scripts/index.js
+++ 01-Dasar-Web/scripts/index.js
@@ -1,8 +1,27 @@
-const aboutContent = [
-    ``
-]
+const aboutContent = [education, skills];
 
+const test = (id) => {
+  Array.from(document.getElementsByClassName('selected')).forEach((item) => {
+    item.classList.remove('selected');
+  });
 
-const test = (value) => {
-  console.log(value);
+  document.getElementById(id).classList.add('selected');
+  let selectedContent;
+
+  switch (id) {
+    case 'education-button':
+      selectedContent = education;
+      break;
+    case 'skills-button':
+      selectedContent = skills;
+      break;
+    default:
+      selectedContent = education;
+  }
+  document.getElementById('about-content').innerHTML = selectedContent;
+};
+
+window.onload = () => {
+  document.getElementById('about-content').innerHTML = education;
+  document.getElementById('education-button').classList.add('selected');
 };
